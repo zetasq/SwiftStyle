276903e6781b891168663562a57b39c5e2030480
Zhu Shengqi <zetasq@gmail.com>
2018-07-29 20:29:05 +0800

    Initial Commit

---
+++ SwiftStyle/SwiftStyle.h
@@ -0,0 +1,19 @@
+//
+//  SwiftStyle.h
+//  SwiftStyle
+//
+//  Created by Zhu Shengqi on 2018/7/29.
+//  Copyright © 2018 Zhu Shengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftStyle.
+FOUNDATION_EXPORT double SwiftStyleVersionNumber;
+
+//! Project version string for SwiftStyle.
+FOUNDATION_EXPORT const unsigned char SwiftStyleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftStyle/PublicHeader.h>
+
+
